63269d2f2eafa4991468f91f77555ddfa6435e6e
Konstantin S. <havendv@gmail.com>
2026-09-13 19:44:20 +0400

    feat: Updated OpenAPI spec (#29)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Vidu/Generated/Vidu.TasksClient.g.cs
+++ src/libs/Vidu/Generated/Vidu.TasksClient.g.cs
@@ -34,7 +34,7 @@ namespace Vidu
         /// <inheritdoc/>
         public global::Vidu.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Vidu.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Vidu.TasksSourceGenerationContext.Default);
 
         /// <summary>
         ///
